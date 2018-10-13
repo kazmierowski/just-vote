895550e254945d1b5443cb8e875c633544f580f2
Kamil Kazmierowski <kamil.kazmierowski@visiondirect.co.uk>
2018-10-13 18:33:07 +0100

    Init commit

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React like a boss !
           </a>
         </header>
       </div>
