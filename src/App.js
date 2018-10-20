98761186ed9cf6ee997ca5808de1d5246fe01402
Kamil Kazmierowski <kamil.kazmierowski@visiondirect.co.uk>
2018-10-20 21:06:19 +0100

    Fix for the full view

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './App.scss';
 import Menu from './Components/SideMenu/SideMenu';
+import Header from './Components/Header/Header';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { routes } from './router';
 
@@ -12,11 +13,7 @@ class App extends Component {
                 <div className="App" id="App-outer-container">
                     <Menu/>
                     <div id="App-page-wrap">
-                        <header className="App-header">
-                            <p>
-                                just vote <span className="App-header-version">v.0.1</span>
-                            </p>
-                        </header>
+                        <Header/>
                         <div className="App-content">
                             {routes.map((route, index) => (
                                 <Route
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './Header.scss';
+
+class Header extends Component {
+
+
+    render() {
+        return(
+            <header className="Header">
+                <p>
+                    just vote <span className="App-header-version">v.0.1</span>
+                </p>
+            </header>
+        )
+    }
+}
+
+export default Header;
